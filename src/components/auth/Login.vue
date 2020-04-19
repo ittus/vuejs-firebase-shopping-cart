ff61a0f153c850a7963cb5e2c320a079e191f8ac
Thang Minh Vu <ittus@users.noreply.github.com>
2020-04-19 15:40:25 +0900

    Fix node 12 build issue (#14)
    * Fix node 12 build issue
    
    * Set node version
    
    * Update firebase version
    
    * Update firebase
    
    * Update other package

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,33 +1,51 @@
 <template>
-<div class="row">
-  <div class="col-md-6 offset-md-3 col-sm-12 offset-sm-1">
-    <form id="login-form" role="form" style="display: block;">
-      <h3 class="text-center">Login</h3>
-      <div class="form-group">
-        <input type="email" name="email" id="email" class="form-control" placeholder="Email Address" v-model="email">
-      </div>
-      <div class="form-group">
-        <input type="password" name="password" id="password" class="form-control" placeholder="Password" v-model="password">
-      </div>
+  <div class="row">
+    <div class="col-md-6 offset-md-3 col-sm-12 offset-sm-1">
+      <form id="login-form" role="form" style="display: block;" @submit.prevent="onSubmit">
+        <h3 class="text-center">Login</h3>
+        <div class="form-group">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            class="form-control"
+            placeholder="Email Address"
+            v-model="email"
+            required
+          />
+        </div>
+        <div class="form-group">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            class="form-control"
+            placeholder="Password"
+            v-model="password"
+            required
+          />
+        </div>
 
-      <div class="form-group">
-        <button class="btn btn-success" style="width: 100%" @click.prevent="loginWithEmailLocal" :disabled="isLoading">
+        <div class="form-group">
+          <button type="submit" class="btn btn-success" style="width: 100%" :disabled="isLoading">
             <i v-if="isLoading" class="fa fa-spinner fa-spin" />
-						Log in
-					</button>
-      </div>
-      <div class="form-group">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="text-center">
-              <router-link to="/register"><a>Register</a></router-link>
+            Log in
+          </button>
+        </div>
+        <div class="form-group">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="text-center">
+                <router-link to="/register">
+                  <a>Register</a>
+                </router-link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -44,7 +62,7 @@ export default {
   },
   methods: {
     ...mapActions(['addMessage', 'clearMessage', 'loginWithEmail']),
-    loginWithEmailLocal() {
+    onSubmit() {
       this.isLoading = true
       let data = {
         email: this.email,
--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -1,27 +1,44 @@
 <template>
   <div class="row">
     <div class="col-md-6 offset-md-3 col-sm-10 offset-sm-1">
-     <form id="register-form" role="form">
+      <form id="register-form" role="form" @submit.prevent="onSubmit">
         <h3 class="text-center">Register</h3>
         <div class="form-group">
-          <input type="email" name="email" id="email" class="form-control" placeholder="Email Address" value=""
-          v-model="email">
+          <input
+            type="email"
+            name="email"
+            id="email"
+            class="form-control"
+            placeholder="Email Address"
+            value
+            v-model="email"
+            required
+          />
         </div>
         <div class="form-group">
-          <input type="password" name="password" id="password" class="form-control" placeholder="Password"
-          v-model="password">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            class="form-control"
+            placeholder="Password"
+            v-model="password"
+            required
+          />
         </div>
         <div class="form-group">
-            <button class="btn btn-success" style="width: 100%" @click.prevent="registerByEmailLocal" :disabled="isLoading">
-              <i v-if="isLoading" class="fa fa-spinner fa-spin" />
-              Register
-            </button>
+          <button type="submit" class="btn btn-success" style="width: 100%" :disabled="isLoading">
+            <i v-if="isLoading" class="fa fa-spinner fa-spin" />
+            Register
+          </button>
         </div>
         <div class="form-group">
           <div class="row">
             <div class="col-lg-12">
               <div class="text-center">
-                <router-link to="/login"><a>Login</a></router-link>
+                <router-link to="/login">
+                  <a>Login</a>
+                </router-link>
               </div>
             </div>
           </div>
@@ -32,39 +49,39 @@
 </template>
 
 <script>
-  import { mapActions } from 'vuex';
-  export default {
-    data() {
-      return {
-        email: '',
-        password: '',
-        isLoading: false
+import { mapActions } from 'vuex';
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      isLoading: false
+    }
+  },
+  methods: {
+    ...mapActions(['clearMessage', 'addMessage', 'registerByEmail']),
+    onSubmit() {
+      this.isLoading = true
+      let data = {
+        email: this.email,
+        password: this.password
       }
-    },
-    methods: {
-       ...mapActions(['clearMessage', 'addMessage', 'registerByEmail']),
-      registerByEmailLocal() {
-        this.isLoading = true
-        let data = {
-          email: this.email,
-          password: this.password
-        }
-        this.registerByEmail(data).then(() => {
-          this.clearMessage();
-          this.$router.push({name: 'mainpage'});
-        })
+      this.registerByEmail(data).then(() => {
+        this.clearMessage();
+        this.$router.push({ name: 'mainpage' });
+      })
         .catch((error) => {
           // console.log('register error', error);
           let message_obj = {
-              message: error.message,
-              messageClass: "danger",
-              autoClose: true
-            }
-           this.addMessage(message_obj);
+            message: error.message,
+            messageClass: "danger",
+            autoClose: true
+          }
+          this.addMessage(message_obj);
         }).then(() => {
           this.isLoading = false
         })
-      }
     }
   }
+}
 </script>
