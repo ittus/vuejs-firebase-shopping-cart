973a4e05bbc03649865e4fe16a4f715b98727e30
ittus <vuminhthang.cm@gmail.com>
2020-04-19 15:36:15 +0900

    Update other package

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,108 +1,126 @@
 <template>
-<div class="container table-responsive">
-  <table id="cart" class="table table-hover table-sm">
-    <thead>
-      <tr>
-        <th style="width:50%">Product</th>
-        <th style="width:10%">Price</th>
-        <th style="width:8%">Quantity</th>
-        <th style="width:22%" class="text-center">Subtotal</th>
-        <th style="width:10%"></th>
-      </tr>
-    </thead>
+  <div class="container table-responsive">
+    <table id="cart" class="table table-hover table-sm">
+      <thead>
+        <tr>
+          <th style="width:50%">Product</th>
+          <th style="width:10%">Price</th>
+          <th style="width:8%">Quantity</th>
+          <th style="width:22%" class="text-center">Subtotal</th>
+          <th style="width:10%"></th>
+        </tr>
+      </thead>
 
-    <transition-group name="list-shopping-cart" tag="tbody">
-      <app-cart-item v-for="cartItem in cartItemList" :cartItem="cartItem" :key="cartItem.id"></app-cart-item>
-    </transition-group>
+      <transition-group name="list-shopping-cart" tag="tbody">
+        <app-cart-item
+          v-for="cartItem in cartItemList"
+          :cartItem="cartItem"
+          :key="cartItem.id"
+        ></app-cart-item>
+      </transition-group>
 
-    <tfoot>
-      <tr class="d-table-row d-sm-none">
-        <td class="text-center"><strong>Total ${{ cartValue }}</strong></td>
-      </tr>
-      <tr>
-        <td>
-          <button class="btn btn-warning" @click="saveShoppingCartLocal">
-							<i class="fa fa-angle-left"></i> Save and Continue Shopping
-						</button>
-        </td>
-        <td colspan="2" class="d-none d-sm-table-cell"></td>
-        <td class="d-none d-sm-table-cell text-center"><strong>Total ${{ cartValue }}</strong></td>
-        <td class="px-0">
-          <button class="btn btn-success" @click="checkout">
-							<span class="text-nowrap">Checkout <i class="fa fa-angle-right d-inline"></i></span>
-					</button>
-        </td>
-      </tr>
-    </tfoot>
-  </table>
-</div>
+      <tfoot>
+        <tr class="d-table-row d-sm-none">
+          <td class="text-center">
+            <strong>Total ${{ cartValue }}</strong>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <button class="btn btn-warning" @click="saveShoppingCartLocal">
+              <i class="fa fa-angle-left"></i> Save and Continue Shopping
+            </button>
+          </td>
+          <td colspan="2" class="d-none d-sm-table-cell"></td>
+          <td class="d-none d-sm-table-cell text-center">
+            <strong>Total ${{ cartValue }}</strong>
+          </td>
+          <td class="px-0">
+            <button class="btn btn-success" @click="checkout">
+              <span class="text-nowrap"
+                >Checkout <i class="fa fa-angle-right d-inline"></i
+              ></span>
+            </button>
+          </td>
+        </tr>
+      </tfoot>
+    </table>
+  </div>
 </template>
 
 <script>
-import {
-  mapActions,
-  mapGetters
-} from 'vuex';
-import CartItem from './cart/CartItem.vue';
+import { mapActions, mapGetters } from 'vuex'
+import CartItem from './cart/CartItem.vue'
 export default {
   computed: {
-    ...mapGetters(['cartItemList', 'isLoggedIn', 'products', 'currentUser', 'cartValue'])
+    ...mapGetters([
+      'cartItemList',
+      'isLoggedIn',
+      'products',
+      'currentUser',
+      'cartValue',
+    ]),
   },
   components: {
-    appCartItem: CartItem
+    appCartItem: CartItem,
   },
   methods: {
-    ...mapActions(['saveShoppingCart', 'addMessage', 'saveToTransaction', 'clearCart']),
+    ...mapActions([
+      'saveShoppingCart',
+      'addMessage',
+      'saveToTransaction',
+      'clearCart',
+    ]),
     checkValidCart(itemList, prodList) {
-      let isValid = true;
-      let message = "";
+      let isValid = true
+      let message = ''
 
-      itemList.map(item => {
+      itemList.map((item) => {
         for (let prodIdx = 0; prodIdx < prodList.length; prodIdx++) {
           if (prodList[prodIdx].id == item.id) {
             if (prodList[prodIdx].quantity < item.quantity) {
-              message = `Only ${prodList[prodIdx].quantity} ${item.title} available in stock`;
-              isValid = false;
-              return;
+              message = `Only ${prodList[prodIdx].quantity} ${item.title} available in stock`
+              isValid = false
+              return
             }
-            break;
+            break
           }
         }
-      });
+      })
       return {
         isValid,
-        message
+        message,
       }
     },
     saveShoppingCartLocal() {
       if (this.isLoggedIn) {
-        let {
-          isValid,
-          message
-        } = this.checkValidCart(this.cartItemList, this.products);
+        let { isValid, message } = this.checkValidCart(
+          this.cartItemList,
+          this.products
+        )
 
         if (isValid) {
           this.saveShoppingCart({
             cartItemList: this.cartItemList,
-            uid: this.currentUser.uid
+            uid: this.currentUser.uid,
           }).then(() => {
             this.addMessage({
               messageClass: 'success',
-              message: 'Your shopping cart is saved successfully'
-            });
-            this.$router.push('/');
-          });
+              message: 'Your shopping cart is saved successfully',
+            })
+            this.$router.push('/')
+          })
         } else {
           this.addMessage({
             messageClass: 'danger',
-            message: message
-          });
+            message: message,
+          })
         }
       } else {
         this.addMessage({
           messageClass: 'warning',
-          message: 'Please login to save your cart'
-        });
+          message: 'Please login to save your cart',
+        })
       }
     },
     checkout() {
@@ -110,45 +128,45 @@ export default {
         if (!this.cartItemList || this.cartItemList.length == 0) {
           this.addMessage({
             messageClass: 'warning',
-            message: 'Your cart is empty!'
-          });
-          return;
+            message: 'Your cart is empty!',
+          })
+          return
         }
-        let {
-          isValid,
-          message
-        } = this.checkValidCart(this.cartItemList, this.products);
+        let { isValid, message } = this.checkValidCart(
+          this.cartItemList,
+          this.products
+        )
 
         if (isValid) {
           this.saveToTransaction({
             cartItemList: this.cartItemList,
-            uid: this.currentUser.uid
+            uid: this.currentUser.uid,
           }).then(() => {
             this.addMessage({
               messageClass: 'success',
-              message: 'Your order has been successfully processed!'
-            });
+              message: 'Your order has been successfully processed!',
+            })
             this.saveShoppingCart({
               cartItemList: [],
-              uid: this.currentUser.uid
-            });
-            this.clearCart();
-            this.$router.push('/');
-          });
+              uid: this.currentUser.uid,
+            })
+            this.clearCart()
+            this.$router.push('/')
+          })
         } else {
           this.addMessage({
             messageClass: 'danger',
-            message: message
-          });
+            message: message,
+          })
         }
       } else {
         this.addMessage({
           messageClass: 'warning',
-          message: 'Please login to checkout'
-        });
+          message: 'Please login to checkout',
+        })
       }
-    }
-  }
+    },
+  },
 }
 </script>
 
@@ -164,7 +182,7 @@ export default {
 
 .table-sm {
   font-size: 0.875rem;
-  /deep/ h4 {
+  ::v-deep h4 {
     font-size: 1.25rem;
   }
 }
